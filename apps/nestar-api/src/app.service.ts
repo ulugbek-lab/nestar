fc59973b3ab89e12f7ccc91b9b06b9296326ae4f
ulugbek-lab <bekievbek02@gmail.com>
2026-08-06 14:54:22 +0900

    feat: integration graphQLModule and build first graphQl query api

--- apps/nestar-api/src/app.service.ts
+++ apps/nestar-api/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Welcome to NESTAR api server';
-  }
+	getHello(): string {
+		return 'Welcome to NESTAR rest api server';
+	}
 }
