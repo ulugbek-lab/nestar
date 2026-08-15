656f8c47451aa3569a6e253a2f79148084ee6c74
ulugbek-lab <bekievbek02@gmail.com>
2026-08-15 16:23:06 +0900

    feat: develop global error graphql, error handling system

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -5,6 +5,7 @@ import { Member } from '../../libs/dto/member/member';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enum';
+import { T } from '../../libs/types/common';
 
 @Injectable()
 export class MemberService {
@@ -16,8 +17,8 @@ export class MemberService {
 			//TODO Auth via token
 			return result;
 		} catch (err) {
-			console.log('Error, Service.model:', err);
-			throw new BadRequestException(err);
+			console.log('Error, Service.model:', (err as T).message);
+			throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
 		}
 	}
 	public async login(input: LoginInput): Promise<Member> {
